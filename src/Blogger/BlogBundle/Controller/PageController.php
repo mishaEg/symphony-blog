ac4776544fecb11ec33afa5617ddb486528f1685
Your Name <you@example.com>
2019-07-02 10:32:40 +0300

    part 2

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -4,8 +4,20 @@ namespace App\Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use Symfony\Component\HttpFoundation\Request;
+use App\Blogger\BlogBundle\Entity\Enquiry;
+use App\Blogger\BlogBundle\Form\EnquiryType;
+
 class PageController extends AbstractController
 {
+
+    private $mailer;
+
+    public function __construct(\Swift_Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
     public function indexAction()
     {
         return $this->render('@BloggerBlog/Page/index.html.twig');
@@ -15,4 +27,36 @@ class PageController extends AbstractController
     {
         return $this->render('@BloggerBlog/Page/about.html.twig');
     }
+
+    public function contactAction(Request $request)
+    {
+        $enquiry = new Enquiry();
+
+        $form = $this->createForm(EnquiryType::class, $enquiry);
+
+        if ($request->isMethod($request::METHOD_POST)) {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                $message = (new \Swift_Message('Contact enquiry from symblog'))
+                    ->setSubject('Contact enquiry from symblog')
+                    ->setFrom('enquiries@symblog.co.uk')
+                    ->setTo($this->getParameter('blogger_blog.emails.contact_email'))
+                    ->setBody($this->renderView('@BloggerBlog/Page/contactEmail.txt.twig', array('enquiry' => $enquiry)));
+
+                $this->mailer->send($message);
+
+                $this->get('session')->getFlashBag()->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+            }
+        }
+
+        return $this->render('@BloggerBlog/Page/contacts.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
 }
